0681ece38495a7b313c5de97ed5426672097d163
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2024-02-05 12:51:36 +0900

    fix:500エラー解消のため、本番ビルド時の設定を変更8

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     // macの場合、serverは不要
-    server: {
-        host: true, // hostを決める必要がある
-        hmr: {
-            host: "localhost", //windows & wsl2の環境だとhostを指定する必要がある
-        },
-    },
+    // server: {
+    //     host: true, // hostを決める必要がある
+    //     hmr: {
+    //         host: "localhost", //windows & wsl2の環境だとhostを指定する必要がある
+    //     },
+    // },
     plugins: [
         laravel({
             input: ["resources/ts/app.tsx"],
