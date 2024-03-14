1ab5646e81cb0c704120f3af1f4cd92b14dc92ea
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2024-03-14 09:11:32 +0900

    feat:必要なページの追加_ファイルだけ作成で中身は未作成

--- resources/ts/app.tsx
+++ resources/ts/app.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-import { TopPage } from "./components/TopPage";
-import { MyPage } from "./components/MyPage";
-import { PostPage } from "./components/PostPage";
+import { TopPage } from "./pages/TopPage";
+import { UserDetail } from "./pages/UserDetail";
+import { PostPage } from "./pages/PostPage";
+import { PostDetail } from "./pages/PostDetail";
+import { PostEdit } from "./pages/PostEdit";
 
 const App = () => {
     const title: string = "Hello TypeScript React";
@@ -13,18 +15,26 @@ const App = () => {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<TopPage />} />
-                    <Route path="/mypage" element={<MyPage />} />
+                    <Route path="/user/detail" element={<UserDetail />} />
                     <Route path="/posts" element={<PostPage />} />
+                    <Route path="/posts/detail" element={<PostDetail />} />
+                    <Route path="/posts/edit" element={<PostEdit />} />
                 </Routes>
                 <ul>
                     <li>
                         <Link to="/">Top</Link>
                     </li>
                     <li>
-                        <Link to="/mypage">My</Link>
+                        <Link to="/user/detail">UserDetail</Link>
                     </li>
                     <li>
                         <Link to="/posts">Post</Link>
+                    </li>
+                    <li>
+                        <Link to="/posts/detail">PostDetail</Link>
+                    </li>
+                    <li>
+                        <Link to="/posts/edit">PostEdit</Link>
                     </li>
                 </ul>
             </BrowserRouter>
