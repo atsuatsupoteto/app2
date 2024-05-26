13e8110f179b30b9d848e38b1cac8d75e83b93b8
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2024-05-26 17:11:49 +0900

    feat:テーブルにテストデータを自動で作成するためにSeederに追記

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,5 +22,14 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+        $user = User::factory()->create();
+        $post = Post::factory()->create([
+            'user_id'=>$user->id
+        ]);
+        Comment::factory()->create([
+            'post_id'=>$post->id,
+            'user_id'=>$user->id,
+        ]);
     }
 }
