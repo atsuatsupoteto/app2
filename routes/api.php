e359c4911e7a1d5f9b02d622e6ebdffe15e66f99
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2024-05-30 22:18:49 +0900

    feat:api.phpにComment用のrouteを追加

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::resource('posts', PostController::class);
+Route::resource('comments', CommentController::class);
 
 //ユーザ認証
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
