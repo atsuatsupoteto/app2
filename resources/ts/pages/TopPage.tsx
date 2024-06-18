17d11d53dad32f80836d3e55f7e2b81ff0fab033
atsuatsupoteto <atsuatsu.poteto@gmail.com>
2024-06-19 08:56:13 +0900

    test:material uiを使って動作確認してみる

--- resources/ts/pages/TopPage.tsx
+++ resources/ts/pages/TopPage.tsx
@@ -1,5 +1,16 @@
+import { Grid } from "@mui/material";
 import React from "react";
+import Post from "../components/TopPage/Post";
 
 export const TopPage = () => {
-    return <div>TopPage</div>;
+    return (
+        <div>
+            <div>TopPage</div>
+            <Grid container spacing={2}>
+                <Grid item xs={8}>
+                    <Post />
+                </Grid>
+            </Grid>
+        </div>
+    );
 };
